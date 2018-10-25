4b20123b5e0a263eebf63701433837d1e170de29
Neb, Sebastian <Sebastian.Neb@senacor.com>
2018-10-25 14:46:17 +0200

    Finalized Bucket scripts

--- scripts/initializeS3Bucket.sh
+++ scripts/initializeS3Bucket.sh
@@ -5,27 +5,13 @@ REGION="eu-central-1"
 
 echo "Erstelle S3 bucket"
 
-aws s3 ls
+aws s3 ls s3://$BUCKET > /dev/null 2>&1
 
-# if [ $? -ne 0 ]; then
-#     echo "Bucket noch nicht vorhanden. Erstelle bucket."
-#     aws s3 mb s3://$BUCKET
+if [ $? -ne 0 ]; then
+    echo "Bucket noch nicht vorhanden. Erstelle bucket."
+    aws s3 mb s3://$BUCKET
 
-#     echo "Adding tag..."
-#     aws s3api put-bucket-tagging \
-#         --bucket $BUCKET \
-#         --tagging 'TagSet=[{Key=CostCenter,Value=Tecco}]' \
-#         --region=$REGION
-
-#     echo "Give AWS time to finalize bucket setup..."
-#     sleep 3
-
-#     echo "Setting up website..."
-#     aws s3 website s3://$BUCKET \
-#         --index-document index.html \
-#         --region=$REGION
-
-#     echo "Done."
-# else
-#     echo "Bucket existiert bereits."
-# fi
+    echo "Bucket erfolgreich erstellt."
+else
+    echo "Bucket existiert bereits."
+fi
